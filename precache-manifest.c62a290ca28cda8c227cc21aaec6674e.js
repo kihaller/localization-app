6b92f9373ae043469ac1e1b2fc0596d2bca04232
kihaller <67028582+kihaller@users.noreply.github.com>
2020-09-30 10:36:55 -0700

    Updates

---
+++ precache-manifest.c62a290ca28cda8c227cc21aaec6674e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622a7446db9d6eaf8ee3a2048e5c9908",
+    "url": "/localization-app/index.html"
+  },
+  {
+    "revision": "76594822aadddca0b0db",
+    "url": "/localization-app/static/css/main.893bad4e.chunk.css"
+  },
+  {
+    "revision": "551b23d174a3f2e4eba5",
+    "url": "/localization-app/static/js/2.25c67e46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/localization-app/static/js/2.25c67e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76594822aadddca0b0db",
+    "url": "/localization-app/static/js/main.cc845eed.chunk.js"
+  },
+  {
+    "revision": "af4a137e6aa1cc67f51a",
+    "url": "/localization-app/static/js/runtime-main.12679824.js"
+  }
+]);
